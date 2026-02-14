91fe5a1647a21f42b40d2303f1bec965f2938929
Podotto <grethelhalphen@gmail.com>
2026-02-13 20:43:55 -0500

    Añadí las distintas condiciones para positivo, negativo y 0.

--- grethel/01_positive_negative_numbers.py
+++ grethel/01_positive_negative_numbers.py
@@ -4,5 +4,13 @@ def main():
     number = int(input("¡Hola! Ingresa un número: "))
     
     #Opciones
+    if number > 0:
+        print("El número es positivo.")
+        
+    elif number < 0:
+        print("El número es negativo.")
+        
+    elif number == 0:
+        print("El número es 0.")
     
 main()
