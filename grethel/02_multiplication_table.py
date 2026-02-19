c233db76af3480c4eeaa907d50c3f2087aca553b
Podotto <grethelhalphen@gmail.com>
2026-02-19 18:40:26 -0500

    Desarrollé la bienvenida y una opción para que el usuario ingrese el número que quiere multiplicar.

---
+++ grethel/02_multiplication_table.py
@@ -0,0 +1,7 @@
+def main():
+   #Bienvenido
+   print("Bienvenido a la Calculadora de Tablas de Multiplicar") 
+   int(input("¿Cuál tabla de multiplicación quieres resolver?: "))
+    
+    
+main()
