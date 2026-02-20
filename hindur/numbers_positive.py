ce1ea55759e86383caad310ce1adb48c4c917f99
Hindur Gonzalez <hindigo9123-cyber>
2026-02-20 18:59:21 -0500

    Numeros positivos para practica 2

---
+++ hindur/numbers_positive.py
@@ -0,0 +1,11 @@
+def ejercicio1():
+    number = float(input("Ingresa un número: "))
+
+    if number > 0:
+        print("El número es POSITIVO ")
+    elif number < 0:
+        print("El número es NEGATIVO ")
+    else:
+        print("El número es CERO ")
+
+main()
