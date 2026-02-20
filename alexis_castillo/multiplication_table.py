5308ec2bedbb1dcf89a8a712a21173a14adb9e5e
acastinaar2002-sys <a.castinaar@gmail.com>
2026-02-19 19:16:34 -0500

    Se implemento una variable condicionada en entero para solicitar un numero entero al usurio,luego se ejecuto una tabla de multiplicar del 1 al 10 usando bucle for, se agrego una variable de resultado condicionada en entero para la multiplicacion del bucle y se imprime la tabla de multiplicar del 1 al 10 que haya solicitado el usuario.

---
+++ alexis_castillo/multiplication_table.py
@@ -0,0 +1,13 @@
+def generate_multiplication_table()-> None:
+
+    number: int = int(input("Agrega un numero cual quisieras saber la tabla del multiplicar del 1 al 10: ")) 
+
+    for i in range(1, 11): 
+
+        result_multiplication: int = number * i
+
+        print(f" {number} x {i} = {result_multiplication} ")
+
+generate_multiplication_table()
+
+
