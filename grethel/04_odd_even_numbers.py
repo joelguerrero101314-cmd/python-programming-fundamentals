f07a1cd884ed26826c1ba1b1ac670d8c88f74a2c
Podotto <grethelhalphen@gmail.com>
2026-02-19 19:55:32 -0500

    Desarrollé la lógica para desarrollar el problema con el operador %

--- grethel/04_odd_even_numbers.py
+++ grethel/04_odd_even_numbers.py
@@ -3,4 +3,10 @@ def main():
     print("¿Par o impar?")
     number = int(input("Ingresa un numero: "))
     
+    if number % 2 == 0:
+        print("El número es par.")
+        
+    else:
+        print("El número es impar.")
+    
 main()
