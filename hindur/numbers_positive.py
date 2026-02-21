446789db3b4674028a945da8299a5ddc18480902
Hindur Gonzalez <hindigo9123-cyber>
2026-02-20 19:49:51 -0500

    Verificacion de signo para nimeros

--- hindur/numbers_positive.py
+++ hindur/numbers_positive.py
@@ -1,11 +1,23 @@
-def ejercicio1():
-    number = float(input("Ingresa un número: "))
+# def main():
+#     number = float(input("Ingresa un número: "))
 
-    if number > 0:
-        print("El número es POSITIVO ")
-    elif number < 0:
-        print("El número es NEGATIVO ")
+#     if number > 0:
+#         print("El número es POSITIVO ")
+#     elif number < 0:
+#         print("El número es NEGATIVO ")
+#     else:
+#         print("El número es CERO ")
+
+# main()
+
+def verificar_signo_numero():
+    number_entry: float = float(input("Ingresa un número: "))
+
+    if number_entry > 0:
+        print("El número es POSITIVO")
+    elif number_entry< 0:
+        print("El número es NEGATIVO")
     else:
-        print("El número es CERO ")
-
-main()
+        print("El número es CERO")
+        
+verificar_signo_numero()
