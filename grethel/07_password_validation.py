f52b4bd8d889dceb29224b10e9978e89a7e2ded5
Podotto <grethelhalphen@gmail.com>
2026-02-20 20:56:47 -0500

    Añadí validación para contraseña.

--- grethel/07_password_validation.py
+++ grethel/07_password_validation.py
@@ -1,8 +1,21 @@
 def main():
-    
-    #Inicio
+
+    # Inicio
     username = input("Ingrese su usuario: ")
-    print("Hola",username)
-    
-    
+    print("Hola", username)
+
+    # Contraseña
+    password = "itse1234"
+
+    # Validación
+    validation = input("Ingrese la contraseña: ")
+
+    # Casos
+    if validation == password:
+        print("Acceso autorizado")
+
+    else:
+        print("Contraseña incorrecta. Intente nuevamente")
+
+
 main()
