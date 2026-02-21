09b2ad7617f3468e895586a0892fd877f5bc7c20
Adolfo-Noel <noelavila30@gmail.com>
2026-02-20 20:23:48 -0500

    arreglando error del creador

--- Adolfo/Table_of_Multiplication.py
+++
@@ -1,9 +0,0 @@
-def main():
-    print("multipliquemos juntos")
-    
-    number = int(input("Escoja el numero que desea multiplicar: "))
-    
-    for i in range (1, 11):
-        Result = number * i
-        print (f"{number} x {i} ={Result}")
-main() 
