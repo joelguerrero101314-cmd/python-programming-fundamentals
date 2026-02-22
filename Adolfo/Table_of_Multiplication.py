323a2580e014c99d97da817cd241e8cc9a32ec5f
Adolfo-Noel <noelavila30@gmail.com>
2026-02-21 19:39:44 -0500

    Guardando avance de table.py

--- Adolfo/Table_of_Multiplication.py
+++ Adolfo/Table_of_Multiplication.py
@@ -1,8 +1,13 @@
 def main():
+    
     print("Multipliquemos juntos")
+    
     number = int(input("Ingrese el numero:"))
+    
     for i in range (1, 11):
+        
         result = number * i
+        
         print(f"{number} x {i} = {result}")
 main()        
     
