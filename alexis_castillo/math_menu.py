e7924283a1fbfc572efa77677cf5cd16ac291be7
acastinaar2002-sys <a.castinaar@gmail.com>
2026-02-21 19:37:46 -0500

    Declare la variable de control y se definio el  bucle while, se agrego las opciones de las operaciones del programa

--- alexis_castillo/math_menu.py
+++ alexis_castillo/math_menu.py
@@ -1,3 +1,13 @@
 def math_menu():
     options : int = 0
     while options != 4:
+        print("Math Menu:")
+        print("1) Add")
+        print("2) Subtract")
+        print("3) Multiply")
+        print("4) Exit")
+        try:
+            options = int(input("Choose an option (1-4): "))
+        except ValueError:
+            print("Please enter a valid integer.")
+            options = 0
