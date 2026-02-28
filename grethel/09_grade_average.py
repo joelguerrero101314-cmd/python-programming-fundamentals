ee2e1ca8924798d0ff47d2f8c1984b9f67b23909
Podotto <grethelhalphen@gmail.com>
2026-02-27 19:31:00 -0500

    Se añade las variables de las notas.

---
+++ grethel/09_grade_average.py
@@ -0,0 +1,13 @@
+def main():
+  
+  #Bienvenida
+  print("Calculadora de Promedio de Notas")
+  
+  #Notas
+  grade_1 = int(input("Ingresa la primera nota: "))
+  grade_2 = int(input("Ingresa la segunda nota: "))
+  grade_3 = int(input("Ingresa la tercera nota: "))
+  grade_4 = int(input("Ingresa la cuarta nota: "))
+  grade_5 = int(input("Ingresa la quinta nota: "))
+  
+main()
