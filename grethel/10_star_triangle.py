c2dacff48013452ad02a8cd75eb38af823af6206
Podotto <grethelhalphen@gmail.com>
2026-02-27 19:53:09 -0500

    Lógica inicial para imprimir las estrellas.

---
+++ grethel/10_star_triangle.py
@@ -0,0 +1,15 @@
+def main():
+  
+  # Bienvenida
+  print("¡Dibujemos estrellas!")
+  
+  # Respuesta del usuario
+  stars = int(input("Ingresa el número de estrellas: "))
+  
+  # Dibujo
+  for i in range(1, stars + 1):
+    print("🌟")
+  
+
+main()
+  
