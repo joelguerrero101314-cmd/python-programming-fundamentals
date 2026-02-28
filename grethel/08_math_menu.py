5e10c82a0c49c4c24efafbb98f99dce6a37c0576
Podotto <grethelhalphen@gmail.com>
2026-02-27 19:13:54 -0500

    Se añade case 2 para la resta.

--- grethel/08_math_menu.py
+++ grethel/08_math_menu.py
@@ -20,6 +20,12 @@ def main():
           number2 = int(input("Ingresa el segundo número: "))
           sum_numbers = number1 + number2
           print(f"La suma es {sum_numbers}")
+          
+        case 2:
+          number1 = int(input("Ingresa el primer número: "))
+          number2 = int(input("Ingresa el segundo número: "))
+          substract_numbers = number1 - number2
+          print(f"La resta es {substract_numbers}")
       
         case 4:
           print("Programa terminado.")
