7eb8dcf73b39f9620abe690646220ee06791cf71
Podotto <grethelhalphen@gmail.com>
2026-02-27 19:37:52 -0500

    Condición para indicar si el usuario aprobó o reprobó.

--- grethel/09_grade_average.py
+++ grethel/09_grade_average.py
@@ -10,4 +10,17 @@ def main():
   grade_4 = int(input("Ingresa la cuarta nota: "))
   grade_5 = int(input("Ingresa la quinta nota: "))
   
+  #Operacion
+  sum_grades = grade_1 + grade_2 + grade_3 + grade_4 + grade_5
+  average = sum_grades/5
+  
+  #Condición
+  if average >= 70:
+    print("Aprobaste la materia.") 
+    print(average)
+    
+  else:
+    print("Reprobaste la materia.")
+    print(average)
+  
 main()
