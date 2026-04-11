fe62a0da2a4027ec6abe5c6fd7f5fe40e7fa4323
hindigo9123-cyber <hindigo.9123@gmail.com>
2026-04-11 04:18:58 -0500

    stretlightinteractive.py reárardo

--- streetlightinteractive.py
+++ streetlightinteractive.py
@@ -10,16 +10,10 @@ sewer_style = """
     margin-top: -50px;
     clip-path: polygon(0% 33%, 65% 10%, 100% 35%, 40% 80%);
     background:
-        liner-gradient(-188deg, var(--sewer-dark) 0 19px, #fff0 20px 100%),
-        liner-gradient( -16deg, var(--sewer-dark) 0 34px, #fff0 35px 100%),
-        liner-gradient( -163deg,var(--sewer-dark) 0 19px, #fff0 20px 100%),
-        liner-gradient( 25deg, var(--sewer-dark) 0 33px, var(--sewer-light) 3 4px 39px, var(--sewer-dark)40px 46px, var(--sewer-light) 47px 52px, var(--sewer-dark) 53px 59px, var(--sewer-light) 60px 67px, var(--sewer-dark) 68px 73px var(--sewer-light) 74px 90px, #fff0 0 100%);
-    
-    
         linear-gradient(-188deg, var(--sewer-dark) 0 19px, #fff0 20px 100%),
-        linear-gradient( -16deg, var(--sewer-dark) 0 34px, #fff0 35px 100%),
-        linear-gradient( -163deg,var(--sewer-dark) 0 19px, #fff0 20px 100%),
-        linear-gradient( 25deg, var(--sewer-dark) 0 33px, var(--sewer-light) 34px 39px, var(--sewer-dark) 40px 46px, var(--sewer-light) 47px 52px, var(--sewer-dark) 53px 59px, var(--sewer-light) 60px 67px, var(--sewer-dark) 68px 73px, var(--sewer-light) 74px 90px, #fff0 0 100%);
+        linear-gradient(-16deg, var(--sewer-dark) 0 34px, #fff0 35px 100%),
+        linear-gradient(-163deg, var(--sewer-dark) 0 19px, #fff0 20px 100%),
+        linear-gradient(25deg, var(--sewer-dark) 0 33px, var(--sewer-light) 34px 39px, var(--sewer-dark) 40px 46px, var(--sewer-light) 47px 52px, var(--sewer-dark) 53px 59px, var(--sewer-light) 60px 67px, var(--sewer-dark) 68px 73px, var(--sewer-light) 74px 90px, #fff0 0 100%);
 }
 
 """
